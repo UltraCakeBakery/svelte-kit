cadabd717e5620ffed5ee1ba43937e4a28f68d92
Jack van der Bilt <jack@managing.software>
2023-03-28 13:36:50 +0000

    refactor!: `src/lib` to `./lib`

--- packages/kit/src/exports/vite/dev/index.js
+++ packages/kit/src/exports/vite/dev/index.js
@@ -261,7 +261,6 @@ export async function dev(vite, vite_config, svelte_config) {
 	const watch = (event, cb) => {
 		vite.watcher.on(event, (file) => {
 			if (
-				file.startsWith(svelte_config.kit.files.routes + path.sep) ||
 				file.startsWith(svelte_config.kit.files.params + path.sep) ||
 				// in contrast to server hooks, client hooks are written to the client manifest
 				// and therefore need rebuilding when they are added/removed
